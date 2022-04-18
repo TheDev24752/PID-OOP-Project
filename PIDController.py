01780ca1e57a1e766c94635efdf596e57ae15ed6
WafflLord <44482057+WafflLord@users.noreply.github.com>
2022-04-18 09:46:32 -0500

    Update PIDController.py

--- PIDController.py
+++ PIDController.py
@@ -1,3 +1,5 @@
+import calculus
+
 class PIDController:
   
   def __init__():
@@ -5,10 +7,15 @@ class PIDController:
     self.ki = 0
     self.kd = 0
     self.setpoint = None
-    self.error_values = []
   
-  def P(sensor_val):
-    return self.kp * (sensor_val - self.setpoint)
+  def P(sensor_val, times):
+    return self.kp * (sensor_val[-1] - self.setpoint)
   
-  def I(sensor_val):
-    
+  def PI(sensor_val, times):
+    return P(sensor_val, times) + calculus.riemann(sensor_val, times)
+  
+  def PD(sensor_val, times):
+    return P(sensor_val, times) + calculus.derivative(sensor_val[-2]. sensor_val[-1], times[-1] - times[-2])
+  
+  def PID(sensor_val, times):
+    return PI(sensor_val, times) + calculus.derivative(sensor_val[-2]. sensor_val[-1], times[-1] - times[-2])
