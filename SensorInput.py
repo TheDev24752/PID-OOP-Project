3877d2671a59084a4bfd7a33230f50561dd05cbf
WafflLord <44482057+WafflLord@users.noreply.github.com>
2022-04-22 09:38:09 -0500

    Update SensorInput.py

--- SensorInput.py
+++ SensorInput.py
@@ -1,10 +1,12 @@
+import pyautogui
+
 class SensorInput:
   
   def __init__():
     self.pin_location
   
   def get_sensor_value():
-    return 1
+    return pyautogui.position()[1]  #gets the mouse's y-position
   
   def get_funct_value(PIDoutput):
     return (PIDoutput * 100 ** 0.5) - 2 #just a random function
