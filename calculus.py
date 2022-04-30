06e2f5ca5de939f3d7e4c9d85b05b681cf8f1350
teelj <jaedonteel@gmail.com>
2022-04-29 22:35:44 -0500

    added some class struct for the presentation
    Merged the new calc class over the old one

--- calculus.py
+++ calculus.py
@@ -1,12 +1,10 @@
-class Calculus:
-    @staticmethod
-    def riemann(data, dx):
-        sigma = 0
-        for y in data:
-            sigma += y * dx
+def riemann(data, dx):
+    sigma = 0
+    for i, y in enumerate(data)):
+        sigma += y * (dx[min(len(dx) - 1, i)] - dx[i])  #dx becomes time between each sensor input, last sensor input is multiplied by 0
 
-        return sigma
+    return sigma
 
-    @staticmethod
-    def derivative(y1, y2, dx):
-        return (y2 - y1) / dx
+
+def derivative(y1, y2, dx):
+    return (y2 - y1) / dx
