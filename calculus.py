564850f31e7e2ead86c80741f537f52b55d091b6
teelj <jaedonteel@gmail.com>
2022-05-01 18:24:19 -0500

    fixed calc class

--- calculus.py
+++ calculus.py
@@ -1,6 +1,6 @@
 def riemann(data, dx):
     sigma = 0
-    for i, y in enumerate(data)):
+    for i, y in enumerate(data):
         sigma += y * (dx[min(len(dx) - 1, i)] - dx[i])  #dx becomes time between each sensor input, last sensor input is multiplied by 0
 
     return sigma
