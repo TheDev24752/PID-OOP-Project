a6857bd3a80d5657c83e6ecd4342a8dcd3e18b87
WafflLord <44482057+WafflLord@users.noreply.github.com>
2022-05-01 18:49:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from tkinter import *
-from tkinter import ttk
-from PID_Auto_Tuning import PIDAutoTuning
-from SensorInput import SensorInput
+import PID_Auto_Tuning
+import SensorInput
+import pyautogui
+import time
 
-pid = PID_Auto_Tuning()
+def changeY(change):
+  pyautogui.moveTo(pyautogui.position()[0], pyautogui.position()[1] + change)
+
+setpoint = 100
+
+sensor = SensorInput()
+controller = PID_Auto_Tuning(setpoint)
+
+controller.ziegler_nicholas_PID(sensor, changeY)
+
+inputs = []
+times = []
+
+inputs.append(sensor.get_sensor_value())
+times.append(time.process_time_ns())
+
+while True:
+  inputs.append(sensor.get_sensor_value())
+  times.append(time.process_time_ns())
+  changeY(controller.PID(inputs, times))
