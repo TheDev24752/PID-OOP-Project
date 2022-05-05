4767d8b1e54b63af0e689035f9f761917abea8ed
WafflLord <44482057+WafflLord@users.noreply.github.com>
2022-05-04 19:15:22 -0500

    Update PID_Auto_Tuning.py

--- PID_Auto_Tuning.py
+++ PID_Auto_Tuning.py
@@ -7,7 +7,7 @@ class PIDAutoTuning(PIDController):
   def __init__(self, setpoint):
     super().__init__(setpoint)
   
-  def ziegler_nichols_PID(self, sensor, output, step_size = 0.1, error_range = 0.1): # pass through sensor object, output method, optionally step size and error
+  def ziegler_nichols(self, sensor, output, step_size, error_range): # pass through sensor object, output method, optionally step size and error
     self.kp = 1
     self.ki = 0
     self.kd = 0
@@ -51,8 +51,25 @@ class PIDAutoTuning(PIDController):
       if max1 >= max2:  # Check that system oscillates constantly
         break
       self.kp += step_size
-    ku = self.kp
-    tu = t2 - t1
+    return (self.kp, t2 - t1)
+  
+  def auto_tune_P(self, sensor, output, step_size = 0.1, error_range = 0.1):
+    self.ziegler_nichols(sensor, output, step_size, error_range)
+    self.kp /= 2
+  
+  def auto_tune_PI(self, sensor, output, step_size = 0.1, error_range = 0.1):
+    ku, tu = self.ziegler_nichols(sensor, output, step_size, error_range)
+    self.kp = 0.45 * ku
+    self.ki = 0.54 * ku / tu
+  
+  def auto_tune_PD(self, sensor, output, step_size = 0.1, error_range = 0.1):
+    ku, tu = self.ziegler_nichols(sensor, output, step_size, error_range)
+    self.kp = 0.8 * ku
+    self.kd = 0.1 * ku * tu
+  
+  def auto_tune_PID(self, sensor, output, step_size = 0.1, error_range = 0.1):
+    ku, tu = self.ziegler_nichols(sensor, output, step_size, error_range)
     self.kp = 0.6 * ku
     self.ki = 1.2 * ku / tu
     self.kd = 0.075 * ku * tu
+    
